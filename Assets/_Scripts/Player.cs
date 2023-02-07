ee73db66eb195f2426203bb5e0d7e27d01fc40b5
Dilara <dilaraismn@gmail.com>
2023-02-08 00:20:54 +0300

    Force Bar Added

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private GameObject bonePrefab;
     [SerializeField] private Transform boneParent;
+    [SerializeField] private Image forceBar;
     private float throwPower = 2000f;
     private float holdDownStartTime;
     private Bone _bone;
@@ -18,8 +20,10 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        currentBone = Instantiate(bonePrefab, boneParent.transform.position,Quaternion.identity, boneParent.transform);
-        _bone = currentBone.GetComponent<Bone>();
+        forceBar =forceBar.GetComponent<Image>();
+        forceBar.fillAmount = 0;
+        
+       CreateBone();
     }
 
     private void Update()
@@ -29,8 +33,13 @@ public class Player : MonoBehaviour
             holdDownStartTime = Time.time;
         }
 
+        if (Input.GetMouseButton(0))
+        {
+            forceBar.fillAmount +=0.0015f;
+        }
         if (Input.GetMouseButtonUp(0))
         {
+            forceBar.fillAmount = 0;
             float holdDownTime = Time.time - this.holdDownStartTime;
             // THROW
             _bone.ThrowBone(CalculateForce(holdDownTime));
@@ -38,11 +47,15 @@ public class Player : MonoBehaviour
         
         if(Bone.canRespawn)
         {
-            currentBone = Instantiate(bonePrefab, boneParent.transform.position,Quaternion.identity, boneParent.transform);
-            _bone = currentBone.GetComponent<Bone>();
+            CreateBone();
         }
     }
 
+    private void CreateBone()
+    {
+        currentBone = Instantiate(bonePrefab, boneParent.transform.position,Quaternion.identity, boneParent.transform);
+        _bone = currentBone.GetComponent<Bone>();
+    }
     private float CalculateForce(float holdTime)
     {
         float maxForceHoldTime = 2f;
