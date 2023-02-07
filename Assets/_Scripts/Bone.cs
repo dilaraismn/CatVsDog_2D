9923d90c05fd676a4f455a789b0e595b4780b9ed
Dilara <dilaraismn@gmail.com>
2023-02-07 23:29:47 +0300

    Throw Completed

--- Assets/_Scripts/Bone.cs
+++ Assets/_Scripts/Bone.cs
@@ -7,15 +7,21 @@ using UnityEngine;
 public class Bone : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    public static bool canRespawn;
 
     private void Awake()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D = this.GetComponent<Rigidbody2D>();
     }
 
     void Start()
     {
-        _rigidbody2D.isKinematic = true;
+    }
+
+    private void OnEnable()
+    {
+        canRespawn = false;
+        this._rigidbody2D.isKinematic = true;
     }
 
     void Update()
@@ -29,5 +35,21 @@ public class Bone : MonoBehaviour
         _rigidbody2D.isKinematic = false;
         var direction = Vector2.up + Vector2.left;
         _rigidbody2D.velocity = direction * force;
+    }
+    
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            canRespawn = true;
+            Destroy(this.gameObject);
+        }
+
+        if (other.CompareTag("Enemy"))
+        {
+            canRespawn = true;
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float throwPower = 500f;
+    [SerializeField] private GameObject bonePrefab;
+    [SerializeField] private Transform boneParent;
+    private float throwPower = 2000f;
     private float holdDownStartTime;
     private Bone _bone;
+    private GameObject currentBone { get; set; }
 
     private void Awake()
     {
-        _bone = FindObjectOfType<Bone>();
+    }
+
+    private void Start()
+    {
+        currentBone = Instantiate(bonePrefab, boneParent.transform.position,Quaternion.identity, boneParent.transform);
+        _bone = currentBone.GetComponent<Bone>();
     }
 
     private void Update()
@@ -24,18 +32,22 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             float holdDownTime = Time.time - this.holdDownStartTime;
-            
             // THROW
             _bone.ThrowBone(CalculateForce(holdDownTime));
+        }
+        
+        if(Bone.canRespawn)
+        {
+            currentBone = Instantiate(bonePrefab, boneParent.transform.position,Quaternion.identity, boneParent.transform);
+            _bone = currentBone.GetComponent<Bone>();
         }
     }
 
     private float CalculateForce(float holdTime)
     {
-        float maxForceHoldTime = 1f;
+        float maxForceHoldTime = 2f;
         float holdTimeNormalized = Mathf.Clamp01(holdTime / maxForceHoldTime);
-        float throwForce = holdTimeNormalized * throwPower * 60;
-        print(throwForce);
+        float throwForce = holdTimeNormalized * throwPower;
         return throwForce;
     }
 }
