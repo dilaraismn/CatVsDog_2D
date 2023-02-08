3121f40f104ad5a73506f8c719396fff5ec4554a
Dilara <dilaraismn@gmail.com>
2023-02-08 10:55:03 +0300

    Health Bar Added

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -2,23 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Image healthBar;
     private Animator _animator;
+    private float health;
+    
 
     private void Awake()
     {
         _animator = GetComponentInChildren<Animator>();
+        healthBar = healthBar.GetComponent<Image>();
     }
 
     void Start()
     {
-        
+        health = 100;
     }
 
     void Update()
     {
+        if (health <= 0)
+        {
+            _animator.Play("Dead");
+            StartCoroutine(Dead());
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -26,6 +36,16 @@ public class Enemy : MonoBehaviour
         if (other.CompareTag("Bone"))
         {
             _animator.Play("Hurt");
+            health -= 40; //20
+            healthBar.fillAmount = (health / 100);
         }
+    }
+
+    IEnumerator Dead()
+    {
+        yield return new WaitForSeconds(.5f);
+        this.enabled = false;
+        //TO DO: WIN UI
+        //TO DO: GAME END
     }
 }
